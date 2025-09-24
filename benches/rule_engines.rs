23d5d77735b4501a2c0fc192a797230de913147a
Ammar Bandukwala <ammar@ammar.io>
2025-09-23 20:28:45 -0500

    WIP

---
+++ benches/rule_engines.rs
@@ -0,0 +1,50 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use httpjail::rules::{
+    RuleEngine, proc::ProcRuleEngine, shell::ShellRuleEngine, v8_js::V8JsRuleEngine,
+};
+use hyper::Method;
+use pprof::criterion::{Output, PProfProfiler};
+use std::time::Duration;
+
+fn bench_v8_js_engine(c: &mut Criterion) {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let engine = V8JsRuleEngine::new("true".to_string()).unwrap();
+    let engine = RuleEngine::from_trait(Box::new(engine), None);
+
+    c.bench_function("v8_js_engine", |b| {
+        b.to_async(&runtime)
+            .iter(|| async { engine.evaluate(Method::GET, "https://example.com").await });
+    });
+}
+
+fn bench_shell_engine(c: &mut Criterion) {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let engine = ShellRuleEngine::new("true".to_string());
+    let engine = RuleEngine::from_trait(Box::new(engine), None);
+
+    c.bench_function("shell_engine", |b| {
+        b.to_async(&runtime)
+            .iter(|| async { engine.evaluate(Method::GET, "https://example.com").await });
+    });
+}
+
+fn bench_proc_engine(c: &mut Criterion) {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let engine = ProcRuleEngine::new("yes true".to_string());
+    let engine = RuleEngine::from_trait(Box::new(engine), None);
+
+    c.bench_function("proc_engine", |b| {
+        b.to_async(&runtime)
+            .iter(|| async { engine.evaluate(Method::GET, "https://example.com").await });
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(10))
+        .with_profiler(PProfProfiler::new(100, Output::Protobuf));
+    targets = bench_v8_js_engine, bench_shell_engine, bench_proc_engine
+}
+
+criterion_main!(benches);
